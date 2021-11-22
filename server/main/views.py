bdc25157f15e40f14e5e9f8fe03f8cdba7714fc7
Эльдар Миннахметов <ciborg.brain@yandex.ru>
2021-11-22 21:12:16 +0500

    Первый коммит, стрим с веб-камеры

---
+++ server/main/views.py
@@ -0,0 +1,44 @@
+import cv2
+import datetime
+import threading
+from django.http import HttpResponse, StreamingHttpResponse
+from django.views.decorators import gzip
+
+def index(request):
+    return HttpResponse(
+        "Работа студента группы РИС-19-1б<br>Эльдара Миннахметова<br>Python Django - %s<br>"
+        "<a href=\"/cam\">Изображение с камеры</a>"
+        % datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    )
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@gzip.gzip_page
+def cam(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:
+        pass
