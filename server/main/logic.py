895699e678e89e661c7fe43bbafebc0dfb5ef0b7
Эльдар Миннахметов <ciborg.brain@yandex.ru>
2021-12-04 22:31:28 +0500

    Рефакторинг, вынес логику в отдельный модуль

---
+++ server/main/logic.py
@@ -0,0 +1,61 @@
+from cv2 import cv2
+from .models import *
+from datetime import datetime, timezone
+
+
+def get_frame():
+    video = cv2.VideoCapture(0)
+    (grabbed, frame) = video.read()
+    _, jpeg = cv2.imencode('.jpg', frame)
+    return jpeg.tobytes()
+
+
+def filter_last_date(date_time, max_waiting):
+    return diff_time(date_time) < max_waiting
+
+
+def diff_time(date_time):
+    return (datetime.now(timezone.utc) - date_time).total_seconds()
+
+
+def to_seconds(date_time):
+    return date_time.timestamp()
+
+
+def attempts_left(name, max_attempts=3, max_waiting=300):
+    attempts = list(Attempt.objects.filter(login=name))
+    attempts = [x for x in attempts if filter_last_date(x.dateTime, max_waiting)]
+    attempts.sort(key=lambda att: att.dateTime)
+    count = len(attempts)
+    if count:
+        seconds = max_waiting - diff_time(attempts[0].dateTime)
+    else:
+        seconds = 0
+    return max_attempts - count, seconds
+
+
+def doesnt_user_exist(login, password=False):
+    if password:
+        users = list(User.objects.filter(
+            login=login,
+            password=password
+        ))
+    else:
+        users = list(User.objects.filter(
+            login=login
+        ))
+    return len(users) == 0
+
+
+def create_attempt(login):
+    Attempt.objects.create(
+        login=login,
+        dateTime=datetime.now(timezone.utc)
+    ).save()
+
+
+def create_user(login, password):
+    User.objects.create(
+        login=login,
+        password=password
+    ).save()
--- server/main/views.py
+++ server/main/views.py
@@ -1,23 +1,18 @@
-import cv2
-from .models import User, Attempt
+from .logic import *
 from django.shortcuts import render
-from datetime import datetime, timezone
 from django.http import HttpResponse, HttpResponseRedirect
 
-
-# Pages
 
 def index(request):
     if request.method == 'GET':
         login = request.session.get('login', '')
-        attempt_login = request.GET.get('login', '')
-        attempts, left = attempts_left(attempt_login)
+        attempt_login = request.GET.get('attempt_login', '')
+        attempt_count, seconds_for_next = attempts_left(attempt_login)
         data = {
-            'response': request.GET.get('response', ''),
-            'attempt_name': attempt_login,
-            'attempts': attempts,
             'login': login,
-            'left': left,
+            'attempt_login': attempt_login,
+            'attempt_count': attempt_count,
+            'seconds_for_next': seconds_for_next,
         }
         return render(request, "index.html", data)
     return HttpResponseRedirect("/")
@@ -34,20 +29,12 @@ def sign_in(request):
         login = request.POST.get("login", "")
         password = request.POST.get("password", "")
         if login != "" and password != "":
-            count, seconds = attempts_left(login)
+            count, _ = attempts_left(login)
             if count == 0:
-                return HttpResponseRedirect("/?login=%s" % login)
-            users = list(User.objects.filter(
-                login=login,
-                password=password
-            ))
-            if len(users) == 0:
-                attempt = Attempt.objects.create(
-                    login=login,
-                    dateTime=datetime.now(timezone.utc)
-                )
-                attempt.save()
-                return HttpResponseRedirect("/?login=%s" % login)
+                return HttpResponseRedirect("/?attempt_login=%s" % login)
+            if doesnt_user_exist(login, password):
+                create_attempt(login)
+                return HttpResponseRedirect("/?attempt_login=%s" % login)
             request.session['login'] = login
     return HttpResponseRedirect("/")
 
@@ -57,12 +44,8 @@ def sign_up(request):
         login = request.GET.get("login", "")
         password = request.GET.get("password", "")
         if login != "" and password != "":
-            if len(list(User.objects.filter(login=login))) == 0:
-                user = User.objects.create(
-                    login=login,
-                    password=password
-                )
-                user.save()
+            if doesnt_user_exist(login):
+                create_user(login, password)
                 request.session['login'] = login
     return HttpResponseRedirect("/")
 
@@ -71,36 +54,3 @@ def sign_out(request):
     if request.method == 'GET':
         request.session.flush()
     return HttpResponseRedirect("/")
-
-
-# Functions
-
-def get_frame():
-    video = cv2.VideoCapture(0)
-    (grabbed, frame) = video.read()
-    _, jpeg = cv2.imencode('.jpg', frame)
-    return jpeg.tobytes()
-
-
-def filter_last_date(date_time, max_waiting):
-    return diff_time(date_time) < max_waiting
-
-
-def diff_time(date_time):
-    return (datetime.now(timezone.utc) - date_time).total_seconds()
-
-
-def to_seconds(date_time):
-    return date_time.timestamp()
-
-
-def attempts_left(name, max_attempts=3, max_waiting=300):
-    attempts = list(Attempt.objects.filter(login=name))
-    attempts = [x for x in attempts if filter_last_date(x.dateTime, max_waiting)]
-    attempts.sort(key=lambda att: att.dateTime)
-    count = len(attempts)
-    if count:
-        seconds = max_waiting - diff_time(attempts[0].dateTime)
-    else:
-        seconds = 0
-    return max_attempts - count, seconds
